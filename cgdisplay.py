1eaf753a1714d11139e2be28b621fd2fe38a6958
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-04-18 09:33:49 +0300

    Update cgdisplay.py

--- cgdisplay.py
+++ cgdisplay.py
@@ -1,40 +1,20 @@
 import time
-import Quartz  # noqa F401
+import Quartz # noqa F401
 import ctypes
 import abc
 import objc
-from typing import Dict
-
-__all__ = ['DisplayCallbackDelegate', ]
 
 
-class _MonitorPanelLoader:
-
-    """
-        ... _MonitorPanelLoader load private framework MonitorPanel and init require class in global variable.
-    """
-    _globals = {}  # define globals dict before loading, it requires if framework fails to load.
-
-    def __init__(self):
-        objc.loadBundle(
-            'MonitorPanel',
-            bundle_path=objc.pathForFramework('/System/Library/PrivateFrameworks/MonitorPanel.framework'),
-            module_globals=globals(),
-        )
-
-    def passAttributes(self) -> Dict:
-        """ Not necessary function, but intuitive understood, if bundle isn't loading.
-        .. versionadded:: 0.0.1"""
-        if self._globals != globals():
-            return globals()
-        return self._globals
-
-
-# create define exceptions and error classes
+# create exceptions and error classes
 
 
 class QErrorNoneAvailable(NotImplementedError):
     pass
+
+
+class QErrorWrongArgument(ValueError):
+    pass
+
 
 class kCGErrorTypes(IOError):  # noqa
     """
@@ -45,65 +25,101 @@ class kCGErrorTypes(IOError):  # noqa
         pass
 
 
+class Helpers:
+    @staticmethod
+    def err_to_exception(err):
+        query_exc = {
+             0: '_kCGErrorSuccess',
+             1000: '_kCGErrorFailure',
+             1001: '_kCGErrorIllegalArgument',
+             1011: '_kCGErrorNoneAvailable',
+             1970170734: '_kDisplayVendorIDUnknown',
+             1007: '_kCGErrorRangeCheck'
+
+
+        }
+        return query_exc[err]
+
+
 class CGDisplayDelegate:
     def __init__(self, display: Quartz.CGMainDisplayID() = 0) -> None:
+        self.display = display or Quartz.CGMainDisplayID()  # define main display reference in abc class.
 
-        for k, v in globals().items():
-            setattr(self, k, v)
-
-        self.display = Quartz.CGMainDisplayID() or display  # define main display reference in abc class.
         self._mp_display = None
         self.status: kCGErrorTypes = kCGErrorTypes(0)
 
     def __abs__(self):
         raise NotImplementedError
 
+    def _load(self):
+        raise NotImplementedError
+
     @abc.abstractmethod
-    def hideCursor(self):
+    def _hideCursor(self):
         pass
 
     @abc.abstractmethod
-    def moveTo(self, x, y):
+    def _moveTo(self, x, y):
         pass
 
     @abc.abstractmethod
-    def setDisplayMode(self, modeIndex):
+    def _setDisplayMode(self, modeIndex):
         pass
 
     @abc.abstractmethod
-    def setTransfer(self):
+    def _setTransfer(self):
         pass
 
     @abc.abstractmethod
-    def setPalette(self):
+    def _setPalette(self):
         pass
 
     @abc.abstractmethod
-    def setRotation(self, angle):
+    def _setRotation(self, angle):
         pass
 
     @abc.abstractmethod
-    def defaultMode(self):
+    def _defaultMode(self):
         pass
 
     @abc.abstractmethod
-    def setDisplayBrightness(self, value):
+    def _setDisplayBrightness(self, value):
         pass
 
     @abc.abstractmethod
-    def displayRotation(self):
+    def _displayRotation(self):
         pass
 
     @abc.abstractmethod
-    def vendorNumber(self):
+    def _vendorNumber(self):
         pass
 
     @abc.abstractmethod
-    def setMirror(self):
+    def _setMirror(self):
+        pass
+
+    @abc.abstractmethod
+    def _getGamma(self):
+        pass
+
+    @abc.abstractmethod
+    def _setGamma(self, red, blue, green):
+        pass
+
+    @abc.abstractmethod
+    def _modeRetain(self):
+        pass
+
+    @abc.abstractmethod
+    def _isDisplay(self, n):
+        pass
+
+    @abc.abstractmethod
+    def _bestDisplayMode(self):
         pass
 
 
-class DisplayCallbackDelegate(CGDisplayDelegate):
+class _DisplayCallbackDelegate(CGDisplayDelegate):
 
     """
         ... DisplayCallbackDelegate can interact with
@@ -116,47 +132,79 @@ class DisplayCallbackDelegate(CGDisplayDelegate):
     _kCGErrorFailure: kCGErrorTypes = 1000
     _kCGErrorIllegalArgument: kCGErrorTypes = 1001
     _kCGErrorNoneAvailable: kCGErrorTypes = 1011
-    _kDisplayVendorIDUnknown = 1970170734  # defined in IOGraphicsTypes.h
+    _kDisplayVendorIDUnknown: kCGErrorTypes = 1970170734  # defined in IOGraphicsTypes.h
+    _kCGErrorRangeCheck: kCGErrorTypes = 1007
+    _kDisplayModeNativeFlag = 33554432  # defined in bridge.bridgesupport and in IOGraphicsTypes.h
 
-    def hideCursor(self) -> _kCGErrorSuccess:
+    def __int__(self):
+        return self.display
+
+    def __le__(self, other):
+        return self.display >= 0
+
+    def __lt__(self, other):
+        return self.display <= 0
+
+    def __repr__(self):
+        return '<' + _DisplayCallbackDelegate.__name__.__repr__() + f' with display at {repr(self.display)} index>'
+
+    def _hideCursor(self) -> _kCGErrorSuccess:
         """ Hides the mouse cursor, and increments the hide cursor count.
+                Mac Catalyst 13.1+
+                macOS 10.0+
             .. versionadded:: 0.0.1 """
-        hide = Quartz.CGDisplayHideCursor(self.display)
-        return hide
+        self.status = Quartz.CGDisplayHideCursor(self.display)
+        time.sleep(0.05)
+        # make delay between executing function, it needs if function is called for a continuous times.
 
-    def moveTo(self, x, y) -> _kCGErrorSuccess:
+        return self.status
+
+    def _moveTo(self, x, y) -> _kCGErrorSuccess:
         """ Moves the mouse cursor to a specified point
         relative to the display origin (the upper left corner of the display).
+                Mac Catalyst 13.1+
+                macOS 10.0+
             .. versionadded:: 0.0.1
+            ..versionupdate:: 0.0.3
         """
-        status = Quartz.CGDisplayMoveCursorToPoint(
+        w, h = Quartz.CGDisplayBounds(self.display).size
+        if x >= w or y >= h:
+            return self._kCGErrorIllegalArgument
+
+        self.status = Quartz.CGDisplayMoveCursorToPoint(
             self.display,
             Quartz.CGRect(x, y)
         )
-        if status == self._kCGErrorSuccess:
+        if self.status == self._kCGErrorSuccess:
             return
-        return status
+        return self.status
 
-    def setDisplayMode(self, modeIndex) -> _kCGErrorSuccess:
+    def _setDisplayMode(self, modeIndex) -> _kCGErrorSuccess:
         """ Switches a display to a different mode.
-            .. versionadded:: 0.0.1 """
-        modes = Quartz.CGDisplayAvailableModes(self.display)[modeIndex]
-        if modeIndex <= 0 or modeIndex >= len(modes):
-            return self._kCGErrorIllegalArgument
+        In an update 0.0.3 I prefer use a MPDisplay for switches modes, because it sets the modes for a long time.
+            .. versionadded:: 0.0.1
+            .. versionupdate:: 0.0.3"""
+        _isLoadMp = self._load()
+        if _isLoadMp:
+            display = _isLoadMp['MPDisplay'].alloc().initWithCGSDisplayID_(self.display)
+            modes = display.allModes()
+            if modeIndex <= 0 or modeIndex >= len(modes):
+                return self._kCGErrorIllegalArgument
 
-        status = Quartz.CGDisplaySwitchToMode(
-            self.display,
-            modes
+            _mode = modes[modeIndex]
+            self.status = display.setMode_(_mode)
 
-        )
-        if status == self._kCGErrorSuccess:
-            return
-        return status
+            if self.status == self._kCGErrorSuccess:
+                return self._kCGErrorSuccess
+            return self.status
 
-    def setTransfer(self) -> _kCGErrorSuccess:
+    def _setTransfer(self) -> _kCGErrorSuccess:
         """ Sets the byte values in the 8-bit RGB gamma tables for a display.
-            .. versionadded:: 0.0.1 """
-        def _tables():
+                Mac Catalyst 13.1+
+                macOS 10.0+
+                .. versionadded:: 0.0.1
+        """
+        def tables():
             table_size = Quartz.kCGDisplayEnabledFlag
             red_table = (ctypes.c_ubyte * table_size)()
             green_table = (ctypes.c_ubyte * table_size)()
@@ -164,15 +212,15 @@ class DisplayCallbackDelegate(CGDisplayDelegate):
             # Create rgb range and pass for 3 last arguments color values
 
             for rgb in range(table_size):
-                red_table[rgb] = rgb
-                green_table[rgb] = rgb
-                blue_table[rgb] = rgb
+                red_table[rgb] = ctypes.c_ubyte(rgb * 3)
+                green_table[rgb] = ctypes.c_ubyte(rgb * 4)
+                blue_table[rgb] = ctypes.c_ubyte(rgb * 5)
                 return red_table, green_table, blue_table
 
         self.status = Quartz.CGSetDisplayTransferByByteTable(
             self.display,
-            len((ctypes.c_ubyte * Quartz.kCGDisplayEnabledFlag)()),
-            *_tables()
+            1,
+            *tables()
 
 
         )
@@ -180,51 +228,71 @@ class DisplayCallbackDelegate(CGDisplayDelegate):
         # make delay between executing function, it needs if function is called for a continuous times.
 
         if self.status == self._kCGErrorSuccess:
-            return
+            return self._kCGErrorSuccess
         return self.status
 
-    def setPalette(self) -> _kCGErrorSuccess:
+    def _load(self):
+        _globals = {}  # define globals dict before loading, it requires if framework fails to load.
+        objc.loadBundle(
+            'MonitorPanel',
+            bundle_path=objc.pathForFramework('/System/Library/PrivateFrameworks/MonitorPanel.framework'),
+            module_globals=globals(),
+        )
+        if _globals != globals():
+            return globals()
+        return _globals  # pragma: no cover
+
+    def _setPalette(self) -> _kCGErrorSuccess:
         """Sets the palette for a display.
             .. versionadded:: 0.0.1
         """
+        if Quartz.CGDisplayCanSetPalette(self.display):
+            self.status = Quartz.CGDisplaySetPalette(
+                self.display,
+                None
+            )
 
-        self.status = Quartz.CGDisplaySetPalette(
-            self.display,
-            None
-        )
+            if self.status == self._kCGErrorNoneAvailable:
+                raise QErrorNoneAvailable(
+                    f'Sorry, {self._setPalette.__name__}(:_:) currently not supported by this OS version.')
 
-        if self.status == self._kCGErrorNoneAvailable:
-            raise QErrorNoneAvailable(
-                f'Sorry, {self.setPalette.__name__!r}(:_:) currently not supported by this OS version.')
+            elif self.status == self._kCGErrorSuccess:
+                return self._kCGErrorSuccess
+        return self._kCGErrorNoneAvailable
 
-        elif self.status == self._kCGErrorSuccess:
-            return
-
-    def setRotation(self, angle) -> _kCGErrorSuccess:
+    def _setRotation(self, angle) -> _kCGErrorSuccess:
         """Rotate display at determine angle multiple to 90.
             .. versionadded:: 0.0.1
         """
+        _isLoadMp = self._load()
 
-        _isLoad = _MonitorPanelLoader().passAttributes()
-        if _isLoad:
-            self._mp_display = _isLoad['MPDisplay'].alloc().initWithCGSDisplayID_(self.display)
-            return self._mp_display
+        if _isLoadMp:
+            self._mp_display = _isLoadMp['MPDisplay'].alloc().initWithCGSDisplayID_(self.display)
+            _isCan = bool(self._mp_display.canChangeOrientation())
+            if _isCan:
+                self.status = self._mp_display.setOrientation_(angle)
+                if self.status == self._kCGErrorSuccess:
+                    return self._kCGErrorSuccess
+                elif not angle % 90 == 0:
+                    return self._kCGErrorIllegalArgument
+                return self.status  # real unknown status
+            return None
+        raise QErrorNoneAvailable(
+            'MonitorPanel framework failed to loading'
+        )
 
-        _isCan = bool(self._mp_display.canChangeOrientation())
-        if _isCan:
-            self.status = self._mp_display.setOrientation_(angle)
-            if self.status == self._kCGErrorSuccess:
-                return
-            elif not angle % 90 == 0:
-                return self._kCGErrorIllegalArgument
-            return self.status  # real unknown status
-
-    def defaultMode(self):
+    def _defaultMode(self) -> _kCGErrorSuccess:
         """Create desc with summary values of init display.
             .. versionadded:: 0.0.1 """
-        pass
+        _isLoadMp = self._load()
 
-    def setDisplayBrightness(self, value) -> _kCGErrorSuccess:
+        if _isLoadMp:
+            self._mp_display = _isLoadMp['MPDisplay'].alloc().initWithCGSDisplayID_(self.display)
+            if self._mp_display.defaultMode():
+                return self._mp_display.defaultMode()
+            return self._kCGErrorNoneAvailable
+
+    def _setDisplayBrightness(self, value) -> _kCGErrorSuccess:
         """regulating brightness of screen to value
             .. versionadded:: 0.0.1
         """
@@ -236,46 +304,134 @@ class DisplayCallbackDelegate(CGDisplayDelegate):
         finally:
             return self._kCGErrorSuccess
 
-    def displayRotation(self) -> _kCGErrorSuccess:
+    def _displayRotation(self) -> _kCGErrorSuccess:
         """
         Returns the rotation angle of a display in degrees.
+             .. Mac Catalyst 13.1+
+            .. macOS 10.5+
             .. versionadded:: 0.0.1
         """
         angle = Quartz.CGDisplayRotation(self.display)
         return int(angle)
 
-    def vendorNumber(self):
+    def _vendorNumber(self) -> _kCGErrorSuccess:
         """
         Returns the vendor number of the specified display's monitor.A vendor number for the monitor associated with
          the specified display, or a constant to indicate an exception.
-
-        .. versionadded:: 0.0.1
+            .. Mac Catalyst 13.1+
+            .. macOS 10.2+
+            .. versionadded:: 0.0.1
         """
         _v_number = Quartz.CGDisplayVendorNumber(self.display)
         if hex(_v_number) == 0xFFFFFFFF:  # If there is no monitor associated with the display
             return None
-        elif _v_number == self._kDisplayVendorIDUnknown:  # If I/O Kit cannot identify the monitor,
+        elif _v_number == self._kDisplayVendorIDUnknown:  # If I/O Kit cannot identify the monitor
             # kDisplayVendorIDUnknown is returned
-            return None
+            return self._kDisplayVendorIDUnknown
         return _v_number
 
-    def setMirror(self):
+    def _setMirror(self) -> _kCGErrorSuccess:
         """Changes the configuration of a mirroring set.
-        .. versionadded:: 0.0.1 """
+            .. Mac Catalyst 13.1+
+            .. macOS 10.2+
+            .. versionadded:: 0.0.1 """
         err, config = Quartz.CGBeginDisplayConfiguration(None)
         Quartz.CGConfigureDisplayMirrorOfDisplay(config, self.display, Quartz.kCGNullDirectDisplay)
-        options = Quartz.kCGConfigureForSession & Quartz.kCGConfigureForAppOnly
+
+        options = (Quartz.kCGConfigureForSession & Quartz.kCGConfigureForAppOnly)
 
         self.status = Quartz.CGCompleteDisplayConfiguration(config, options)
         if self.status == self._kCGErrorSuccess:
-            return
+            return self._kCGErrorSuccess
         elif self.status == self._kCGErrorIllegalArgument:
             return None
         return self.status
 
+    def _getGamma(self) -> _kCGErrorSuccess:
+        """
+        Gets the coefficients of the gamma transfer formula for a display.
+            .. Mac Catalyst 13.1+
+            .. macOS 10.0+
+            .. versionadded:: 0.0.2
+        """
+        self.status, *disp_trans = Quartz.CGGetDisplayTransferByFormula(
+            self.display,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        if self.status == self._kCGErrorSuccess:
+            return disp_trans
+        if self.status == self._kCGErrorIllegalArgument:
+            return None
+        return self.status
 
+    def _setGamma(self, red, blue, green) -> _kCGErrorSuccess:
+        """
+        Sets the gamma function for a display, by specifying the coefficients of the gamma transfer formula.
+            ..Mac Catalyst 13.1+
+            ..macOS 10.0+
+            .. versionadded:: 0.0.3
+        """
+        if all(color == red for color in [blue, green]):
+            return self._kCGErrorIllegalArgument
+            # if all gamuts are equal, it means that the RGB gamma of the display will not change.
 
+        self.status = Quartz.CGSetDisplayTransferByFormula(
+            self.display,
+            0.0, 1.0, red,
+            0.0, 1.0, green,
+            0.0, 1.0, blue
+        )
+        time.sleep(0.02)
+        # make delay between executing function, it needs if function is called for a continuous times.
+        if self.status == self._kCGErrorSuccess:
+            return self._kCGErrorSuccess
 
+        if self.status == self._kCGErrorRangeCheck:
+            return None
+        return self.status
 
+    def _modeRetain(self) -> _kCGErrorSuccess:
+        """Retains a Core Graphics display mode.
+            Mac Catalyst 13.1+
+            macOS 10.6+
+         .. versionadded:: 0.0.4
+        """
 
+        display_modes = Quartz.CGDisplayCopyAllDisplayModes(self.display, None)
 
+        for md in range(Quartz.CFArrayGetCount(display_modes)):
+            cur_mode = Quartz.CFArrayGetValueAtIndex(display_modes, md)
+
+            if Quartz.CGDisplayModeGetIOFlags(cur_mode) & self._kDisplayModeNativeFlag:
+                io_port = Quartz.CGDisplayModeGetIOFlags(cur_mode)
+                return Quartz.CGDisplayModeRetain(io_port)
+            return None
+
+    def _isDisplay(self, n) -> _kCGErrorSuccess:
+        _listDisplay = Quartz.CGGetActiveDisplayList(30, None, None)
+        return _listDisplay
+
+    def _bestDisplayMode(self) -> _kCGErrorSuccess:
+        """
+        Returns information about the display mode closest to a specified depth, screen size, and refresh rate.
+            Mac Catalyst 13.1-13.3
+            ..versionadded:: 0.0.4
+        """
+        mode, self.status = Quartz.CGDisplayBestModeForParametersAndRefreshRate(
+            Quartz.CGMainDisplayID(),
+            0,
+            Quartz.CGDisplayBitsPerPixel(Quartz.CGMainDisplayID()),
+            *Quartz.CGDisplayBounds(Quartz.CGMainDisplayID()).size,
+            None
+        )
+        if self.status == self._kCGErrorSuccess:
+            return mode
+        return self.status
